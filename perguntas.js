c662030b68b89aef9992c21ae43853537530c31a
leonardo52006 <00001094233742sp@al.educacao.sp.gov.br>
2024-12-02 11:03:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Futebol',
+    'Qual é a maior rivalidade do futebol brasileiro?',
+    'A maior rivalidade do futebol brasileiro é entre Flamengo e Vasco, conhecida como Clássico dos Milhões'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a maior rivalidade em São Paulo?',
+    'A maior rivalidade em São Paulo é entre Corinthians e Palmeiras, conhecida como Derby Paulista'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a maior rivalidade no Rio de Janeiro?',
+    'A maior rivalidade no Rio de Janeiro é entre Flamengo e Fluminense, conhecida como Fla-Flu'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a maior rivalidade no Rio Grande do Sul?',
+    'A maior rivalidade no Rio Grande do Sul é entre Grêmio e Internacional, conhecida como Gre-Nal'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a maior rivalidade em Minas Gerais?',
+    'A maior rivalidade em Minas Gerais é entre Atlético-MG e Cruzeiro, conhecida como o Clássico Mineiro'
+)
